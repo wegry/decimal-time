143e583156b39537690b95ab26623ab9617aa277
Zach Wegrzyniak <zwegrzyniak@gmail.com>
2020-03-29 22:06:48 -0600

    Use date-fns in lieu of luxon

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-import { DateTime, Settings } from 'luxon'
+import { differenceInMilliseconds, startOfDay } from 'date-fns'
+import { toDate, utcToZonedTime } from 'date-fns-tz'
+
 import './index.scss'
 import * as _ from 'lodash-es'
 
 const STANDARD_SECONDS_PER_MINUTE = 60
-const STANDARD_SECONDS_PER_HOUR = 60 * STANDARD_SECONDS_PER_MINUTE
 const STANDARD_SECONDS_PER_DAY = 86400
 const DECIMAL_SECONDS_PER_DAY = 100000
+const STANDARD_MILLISECONDS_PER_DAY = STANDARD_SECONDS_PER_DAY * 1000
 
 function renderMarkers() {
   const ticks = document.querySelector('.ticks')
@@ -37,18 +39,17 @@ function padTime(time) {
   return String(Math.floor(time)).padStart(2, '0')
 }
 
+const timeZone = 'Europe/Paris'
+
 function conversion(time) {
-  const { second, minute, hour, millisecond } = time
-  // Can't use Date.prototype.getTime here because of UTC v. CEST
-  const decimalized =
-    (second +
-      STANDARD_SECONDS_PER_MINUTE * minute +
-      STANDARD_SECONDS_PER_HOUR * hour) /
-    STANDARD_SECONDS_PER_DAY
-  const dHours = decimalized * 10
-  const dMinutes = (decimalized * 1000) % 100
-  const dSeconds =
-    (decimalized * DECIMAL_SECONDS_PER_DAY + millisecond / 1000) % 100
+  const dayStart = startOfDay(time)
+  const decimalized = differenceInMilliseconds(time, dayStart)
+  // console.log(time, dayStart)
+
+  const dayPortionPassed = decimalized / STANDARD_MILLISECONDS_PER_DAY
+  const dHours = dayPortionPassed * 10
+  const dMinutes = (dayPortionPassed * 10 * 100) % 100
+  const dSeconds = (dayPortionPassed * DECIMAL_SECONDS_PER_DAY) % 100
 
   return {
     dHours,
@@ -57,11 +58,9 @@ function conversion(time) {
   }
 }
 
-Settings.defaultZoneName = 'Europe/Paris'
-
 function rotation(amount, outOf, options) {
-  const { log = false, smoothing = false } = options || {}
-  const smoothed = smoothing ? Math.floor(amount) : amount,
+  const { log = false, snap = true } = options || {}
+  const smoothed = snap ? Math.floor(amount) : amount,
     rotation = (smoothed / outOf) * 360
 
   if (log) {
@@ -72,9 +71,9 @@ function rotation(amount, outOf, options) {
 
 function rotations({ dHours, dMinutes, dSeconds }) {
   return {
-    hoursRotation: rotation(dHours, 10),
-    minutesRotation: rotation(dMinutes, 100),
-    secondsRotation: rotation(dSeconds, 100, { smoothing: true })
+    hoursRotation: rotation(dHours, 10, { snap: false }),
+    minutesRotation: rotation(dMinutes, 100, { snap: false }),
+    secondsRotation: rotation(dSeconds, 100, { snap: true })
   }
 }
 
@@ -92,7 +91,8 @@ const timeDisplay = _.once(() => {
 
 // https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
 function updateTime() {
-  const time = DateTime.local()
+  const time = utcToZonedTime(new Date(), { timeZone })
+
   const { dHours, dMinutes, dSeconds } = conversion(time)
   const { hoursRotation, minutesRotation, secondsRotation } = rotations({
     dHours,
